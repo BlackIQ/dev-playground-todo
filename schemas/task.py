cf12e8b9e4a54c246faddf25243add07b7f6400b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-11 12:49:59 +0330

    Using enum for priority

--- schemas/task.py
+++ schemas/task.py
@@ -1,12 +1,24 @@
+# Enum
+from enum import IntEnum
+
 # BaseSchema
 from database.base import BaseSchema
+
+
+# Enum of Task priority
+class TaskPriority(IntEnum):
+    VERY_LOW = 1
+    LOW = 2
+    MEDIUM = 3
+    HIGH = 4
+    VERY_HIGH = 5
 
 
 # Task Schema
 class Task(BaseSchema):
     title: str
     details: str
-    priority: int
+    priority: TaskPriority
     is_completed: bool
 
 

b64a06dece5a9c132273bf795b13a3c05cc3c9c7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-11 11:37:54 +0330

    Add base of schemas

--- database/base.py
+++ database/base.py
@@ -1,7 +1,13 @@
-# SQLAlchemy DeclarativeBase
+# SQLAlchemy & Pydantic
+from pydantic import BaseModel, ConfigDict
 from sqlalchemy.orm import DeclarativeBase
 
 
-# The Base class we use over all of out models
+# Base class for all SQLAlchemy models
 class Base(DeclarativeBase):
     pass
+
+
+# Base class for all Pydantic schemas
+class BaseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)

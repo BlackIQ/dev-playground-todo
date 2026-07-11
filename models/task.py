c4ea5d2f2b97761cb4b2c6858961122675c1bb61
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-11 11:43:06 +0330

    Create Task Model

---
+++ models/task.py
@@ -0,0 +1,16 @@
+# SQLAlchemy DataTypes
+from sqlalchemy import Column, Integer, Boolean, String
+
+# BaseModel
+from database.base import Base
+
+
+# Task Model
+class Task(Base):
+    __tablename__ = "tasks"  # Table name
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    details = Column(String, nullable=False)
+    priority = Column(Integer, nullable=False)
+    is_completed = Column(Boolean, nullable=False)
